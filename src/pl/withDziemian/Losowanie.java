f168b64cbb42c78263ab87c49f811e47666e93db
mstaniol <mt.staniszewski@gmail.com>
2019-04-15 19:05:03 +0200

    Adding basic c-file

--- src/pl/withDziemian/Losowanie.java
+++ src/pl/withDziemian/Losowanie.java
@@ -6,9 +6,8 @@ public class Losowanie {
     public static void main(String[] args) {
 
         Random random = new Random();
-        String [] leanguages = new String[]{"PHP", "C#", "Kotlin", "Ruby", "Python"};
-        System.out.println(leanguages[random.nextInt(4)]);
-
+        String [] leanguages = new String[]{"PHP", "C#", "Kotlin", "Ruby", "Python", ".NET", "c++", "c", "go", "Lisp"};
+        System.out.println(leanguages[random.nextInt(9)]);
     }
 
 
